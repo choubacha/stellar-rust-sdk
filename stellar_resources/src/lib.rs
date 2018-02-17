898c54987758f5a1096d874e811ff6ed345b6142
Kevin Bacha <chewbacha@gmail.com>
2018-02-17 11:01:02 -0800

    Adds documentation requirements to resources
    Adds a macro to require documentation for public data types and
    functions. Also requires that all warnings are accounted for.

--- stellar_resources/src/lib.rs
+++ stellar_resources/src/lib.rs
@@ -1,8 +1,20 @@
+#![deny(warnings, missing_docs, missing_debug_implementations)]
+//! Defines the basic resources of stellar's horizon end points and
+//! implements their serialization.
+
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
-#[macro_use]
 extern crate serde_json;
 
 mod utils;
 mod account;
+
+/// # Stellar Resources
+///
+/// A collection of data types and resources used within the stellar api.
+/// All the derives for XDR and JSON are implemented for the resources so that
+/// they can be used with a client. Either for reading or for writing.
+pub mod stellar_resources {
+    pub use account::Account;
+}
