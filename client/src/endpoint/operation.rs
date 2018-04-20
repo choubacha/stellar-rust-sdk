05822f22ee376047c8f444ef8ddbd765e6d3fce0
Kevin Choubacha <chewbacha@gmail.com>
2018-04-19 17:13:07 -0700

    Implement Effects for Operation endpoint
    Utilizes the new TryFromUri trait and defines IntoRequest.
    
    closes #55

--- client/src/endpoint/operation.rs
+++ client/src/endpoint/operation.rs
@@ -2,7 +2,7 @@
 use error::Result;
 use http::{Request, Uri};
 use std::str::FromStr;
-use stellar_resources::Operation;
+use stellar_resources::{Effect, Operation};
 use super::{Body, Cursor, Direction, IntoRequest, Limit, Order, Records};
 use uri::{self, TryFromUri, UriWrap};
 
@@ -79,7 +79,7 @@ impl TryFromUri for All {
 }
 
 #[cfg(test)]
-mod all_operationss_tests {
+mod all_operations_tests {
     use super::*;
 
     #[test]
@@ -182,5 +182,148 @@ mod operation_details_tests {
         let req = ep.into_request("https://www.google.com").unwrap();
         assert_eq!(req.uri().path(), "/operations/123");
         assert_eq!(req.uri().query(), None);
+    }
+}
+
+/// The operation effects endpoint provides a set of effects that are associated with a single
+/// operation. The operation ID provided in the id argument specifies which operation to load.
+///
+/// <https://www.stellar.org/developers/horizon/reference/endpoints/effects-for-operation.html>
+///
+/// ## Example
+///
+/// ```
+/// use stellar_client::sync::Client;
+/// use stellar_client::endpoint::{operation, Limit};
+///
+/// let client = Client::horizon_test().unwrap();
+///
+/// // Grab an operation so that we know that we can request one from
+/// // horizon that actually exists.
+/// let all = operation::All::default().with_limit(1);
+/// let all = client.request(all).unwrap();
+///
+/// let operation_id = all.records()[0].id();
+///
+/// let effects = operation::Effects::new(operation_id);
+/// let effects = client.request(effects).unwrap();
+///
+/// # assert!(effects.records().len() > 0);
+/// ```
+#[derive(Debug, Cursor, Limit, Order)]
+pub struct Effects {
+    id: i64,
+    cursor: Option<String>,
+    order: Option<Direction>,
+    limit: Option<u32>,
+}
+
+impl Effects {
+    /// Creates a new endpoint struct for use in requesting the effects for
+    /// an operation.
+    ///
+    /// ## Example
+    /// ```
+    /// use stellar_client::endpoint::operation;
+    ///
+    /// let details = operation::Effects::new(123);
+    /// ```
+    pub fn new(id: i64) -> Effects {
+        Effects {
+            id,
+            cursor: None,
+            order: None,
+            limit: None,
+        }
+    }
+
+    fn has_query(&self) -> bool {
+        self.order.is_some() || self.cursor.is_some() || self.limit.is_some()
+    }
+}
+
+impl IntoRequest for Effects {
+    type Response = Records<Effect>;
+
+    fn into_request(self, host: &str) -> Result<Request<Body>> {
+        let mut uri = format!("{}/operations/{}/effects", host, self.id);
+
+        if self.has_query() {
+            uri.push_str("?");
+
+            if let Some(order) = self.order {
+                uri.push_str(&format!("order={}&", order.to_string()));
+            }
+
+            if let Some(cursor) = self.cursor {
+                uri.push_str(&format!("cursor={}&", cursor));
+            }
+
+            if let Some(limit) = self.limit {
+                uri.push_str(&format!("limit={}", limit));
+            }
+        }
+
+        let uri = Uri::from_str(&uri)?;
+        let request = Request::get(uri).body(Body::None)?;
+        Ok(request)
+    }
+}
+
+impl TryFromUri for Effects {
+    fn try_from_wrap(wrap: &UriWrap) -> ::std::result::Result<Self, uri::Error> {
+        let path = wrap.path();
+        match (path.get(0), path.get(1), path.get(2)) {
+            (Some(&"operations"), Some(id), Some(&"effects")) => {
+                let params = wrap.params();
+                Ok(Self {
+                    id: id.parse()?,
+                    cursor: params.get_parse("cursor").ok(),
+                    order: params.get_parse("order").ok(),
+                    limit: params.get_parse("limit").ok(),
+                })
+            }
+            _ => Err(uri::Error::invalid_path()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod all_effects_tests {
+    use super::*;
+
+    #[test]
+    fn it_leaves_off_the_params_if_not_specified() {
+        let ep = Effects::new(123);
+        let req = ep.into_request("https://www.google.com").unwrap();
+        assert_eq!(req.uri().path(), "/operations/123/effects");
+        assert_eq!(req.uri().query(), None);
+    }
+
+    #[test]
+    fn it_puts_the_query_params_on_the_uri() {
+        let ep = Effects::new(123)
+            .with_cursor("CURSOR")
+            .with_limit(123)
+            .with_order(Direction::Desc);
+        let req = ep.into_request("https://www.google.com").unwrap();
+        assert_eq!(req.uri().path(), "/operations/123/effects");
+        assert_eq!(
+            req.uri().query(),
+            Some("order=desc&cursor=CURSOR&limit=123")
+        );
+    }
+
+    #[test]
+    fn it_parses_query_params_from_uri() {
+        let uri: Uri = "/operations/123/effects?order=desc&cursor=CURSOR&limit=123"
+            .parse()
+            .unwrap();
+
+        let effects = Effects::try_from(&uri).unwrap();
+        assert_eq!(effects.id, 123);
+        assert_eq!(effects.order, Some(Direction::Desc));
+        assert_eq!(effects.cursor, Some("CURSOR".to_string()));
+        assert_eq!(effects.limit, Some(123));
     }
 }
