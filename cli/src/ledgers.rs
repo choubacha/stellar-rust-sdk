4ec6d7f1434c1923e77b2c520c1208720214eded
correlator <scotthoch86@gmail.com>
2018-04-27 21:31:48 -0700

    add payments to the cli

--- cli/src/ledgers.rs
+++ cli/src/ledgers.rs
@@ -1,8 +1,8 @@
-use stellar_client::{endpoint::ledger, resources::OperationKind, sync::{self, Client}};
+use stellar_client::{endpoint::ledger, sync::{self, Client}};
 use clap::ArgMatches;
 use error::Result;
-use super::{cursor, ordering, pager::Pager};
 use fmt::{Formatter, Simple};
+use super::{cursor, ordering, pager::Pager};
 
 pub fn all(client: &Client, matches: &ArgMatches) -> Result<()> {
     let pager = Pager::from_arg(&matches);
@@ -41,63 +41,9 @@ pub fn payments(client: &Client, matches: &ArgMatches) -> Result<()> {
     let iter = sync::Iter::new(&client, endpoint);
 
     let mut res = Ok(());
+    let mut fmt = Formatter::start_stdout(Simple);
     pager.paginate(iter, |result| match result {
-        Ok(op) => {
-            println!("ID:             {}", op.id());
-            match *op.kind() {
-                OperationKind::CreateAccount(ref create_account) => {
-                    println!("Operation Kind:   Create Account");
-                    println!("Account:          {}", create_account.account());
-                    println!("Funder:           {}", create_account.funder());
-                    println!("Starting Balance: {}", create_account.starting_balance());
-                }
-                OperationKind::Payment(ref payment) => {
-                    println!("Operation Kind: Payment");
-                    println!("To account:     {}", payment.to());
-                    println!("From account:   {}", payment.from());
-                    println!("Asset Type:     {}", payment.asset().asset_type());
-                    println!("Asset Code:     {}", payment.asset().code());
-                    println!("Asset Issuer:   {}", payment.asset().issuer());
-                    println!("Amount:         {}", payment.amount());
-                }
-                OperationKind::PathPayment(ref path_payment) => {
-                    println!("Operation Kind:           Path Payment");
-                    println!("To account:               {}", path_payment.to());
-                    println!("From account:             {}", path_payment.from());
-                    println!(
-                        "Source Asset Type:        {}",
-                        path_payment.source_asset().asset_type()
-                    );
-                    println!(
-                        "Source Asset Code:        {}",
-                        path_payment.source_asset().code()
-                    );
-                    println!(
-                        "Source Asset Issuer:      {}",
-                        path_payment.source_asset().issuer()
-                    );
-                    println!("Source Amount:            {}", path_payment.source_amount());
-                    println!(
-                        "Destination Asset Type:   {}",
-                        path_payment.destination_asset().asset_type()
-                    );
-                    println!(
-                        "Destination Asset Code:   {}",
-                        path_payment.destination_asset().code()
-                    );
-                    println!(
-                        "Destination Asset Issuer: {}",
-                        path_payment.destination_asset().issuer()
-                    );
-                    println!(
-                        "Destination Amount:       {}",
-                        path_payment.destination_amount()
-                    );
-                }
-                _ => println!("Something unexpected happened"),
-            }
-            println!();
-        }
+        Ok(operation) => fmt.render(&operation),
         Err(err) => res = Err(err.into()),
     });
     res
--- cli/src/transactions.rs
+++ cli/src/transactions.rs
@@ -1,4 +1,4 @@
-use stellar_client::{endpoint::transaction, resources::OperationKind, sync::{self, Client}};
+use stellar_client::{endpoint::transaction, sync::{self, Client}};
 use clap::ArgMatches;
 use super::{cursor, ordering, pager::Pager};
 use error::Result;
@@ -38,63 +38,9 @@ pub fn payments(client: &Client, matches: &ArgMatches) -> Result<()> {
     let iter = sync::Iter::new(&client, endpoint);
 
     let mut res = Ok(());
+    let mut fmt = Formatter::start_stdout(Simple);
     pager.paginate(iter, |result| match result {
-        Ok(op) => {
-            println!("ID:             {}", op.id());
-            match *op.kind() {
-                OperationKind::CreateAccount(ref create_account) => {
-                    println!("Operation Kind:   Create Account");
-                    println!("Account:          {}", create_account.account());
-                    println!("Funder:           {}", create_account.funder());
-                    println!("Starting Balance: {}", create_account.starting_balance());
-                }
-                OperationKind::Payment(ref payment) => {
-                    println!("Operation Kind: Payment");
-                    println!("To account:     {}", payment.to());
-                    println!("From account:   {}", payment.from());
-                    println!("Asset Type:     {}", payment.asset().asset_type());
-                    println!("Asset Code:     {}", payment.asset().code());
-                    println!("Asset Issuer:   {}", payment.asset().issuer());
-                    println!("Amount:         {}", payment.amount());
-                }
-                OperationKind::PathPayment(ref path_payment) => {
-                    println!("Operation Kind:           Path Payment");
-                    println!("To account:               {}", path_payment.to());
-                    println!("From account:             {}", path_payment.from());
-                    println!(
-                        "Source Asset Type:        {}",
-                        path_payment.source_asset().asset_type()
-                    );
-                    println!(
-                        "Source Asset Code:        {}",
-                        path_payment.source_asset().code()
-                    );
-                    println!(
-                        "Source Asset Issuer:      {}",
-                        path_payment.source_asset().issuer()
-                    );
-                    println!("Source Amount:            {}", path_payment.source_amount());
-                    println!(
-                        "Destination Asset Type:   {}",
-                        path_payment.destination_asset().asset_type()
-                    );
-                    println!(
-                        "Destination Asset Code:   {}",
-                        path_payment.destination_asset().code()
-                    );
-                    println!(
-                        "Destination Asset Issuer: {}",
-                        path_payment.destination_asset().issuer()
-                    );
-                    println!(
-                        "Destination Amount:       {}",
-                        path_payment.destination_amount()
-                    );
-                }
-                _ => println!("Something unexpected happened"),
-            }
-            println!();
-        }
+        Ok(operation) => fmt.render(&operation),
         Err(err) => res = Err(err.into()),
     });
     res
