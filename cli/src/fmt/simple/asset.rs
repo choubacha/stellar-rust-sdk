e53a5360ddd3ed495872584f88c5e145351c4863
correlator <scotthoch86@gmail.com>
2018-04-27 21:37:00 -0700

    Define append macro once for simple formatter
    Each formatter was defining its own append method.  This commit extracts
    that macro into /fmt/simple/mod.rs to DRY up those definitions.

--- cli/src/fmt/simple/asset.rs
+++ cli/src/fmt/simple/asset.rs
@@ -5,25 +5,17 @@ use stellar_client::resources::Asset;
 impl Render<Asset> for Simple {
     fn render(&self, asset: &Asset) -> Option<String> {
         let mut buf = String::new();
-
-        macro_rules! append {
-            ($($args:tt)*) => {
-                buf.push_str(&format!($($args)*));
-                buf.push_str("\n");
-            }
-        }
-
-        append!("Code:         {}", asset.code());
-        append!("Type:         {}", asset.asset_type());
-        append!("Issuer:       {}", asset.issuer());
-        append!("Amount:       {}", asset.amount());
-        append!("Num accounts: {}", asset.num_accounts());
-        append!("Flags:");
+        append_to_buffer!(buf, "Code:         {}", asset.code());
+        append_to_buffer!(buf, "Type:         {}", asset.asset_type());
+        append_to_buffer!(buf, "Issuer:       {}", asset.issuer());
+        append_to_buffer!(buf, "Amount:       {}", asset.amount());
+        append_to_buffer!(buf, "Num accounts: {}", asset.num_accounts());
+        append_to_buffer!(buf, "Flags:");
         if asset.is_auth_required() {
-            append!("  auth is required");
+            append_to_buffer!(buf, "  auth is required");
         }
         if asset.is_auth_revocable() {
-            append!("  auth is revocable");
+            append_to_buffer!(buf, "  auth is revocable");
         }
         Some(buf)
     }
--- cli/src/fmt/simple/trade_aggregation.rs
+++ cli/src/fmt/simple/trade_aggregation.rs
@@ -5,22 +5,15 @@ use stellar_client::resources::TradeAggregation;
 impl Render<TradeAggregation> for Simple {
     fn render(&self, aggregation: &TradeAggregation) -> Option<String> {
         let mut buf = String::new();
-
-        macro_rules! append {
-            ($($args:tt)*) => {
-                buf.push_str(&format!($($args)*));
-                buf.push_str("\n");
-            }
-        }
-        append!("timestamp:      {}", aggregation.started_at());
-        append!("trade_count:    {}", aggregation.count());
-        append!("base_volume:    {}", aggregation.base_volume());
-        append!("counter_volume: {}", aggregation.counter_volume());
-        append!("average:        {}", aggregation.average());
-        append!("high:           {}", aggregation.high());
-        append!("low:            {}", aggregation.low());
-        append!("open:           {}", aggregation.open());
-        append!("close:          {}", aggregation.close());
+        append_to_buffer!(buf, "timestamp:      {}", aggregation.started_at());
+        append_to_buffer!(buf, "trade_count:    {}", aggregation.count());
+        append_to_buffer!(buf, "base_volume:    {}", aggregation.base_volume());
+        append_to_buffer!(buf, "counter_volume: {}", aggregation.counter_volume());
+        append_to_buffer!(buf, "average:        {}", aggregation.average());
+        append_to_buffer!(buf, "high:           {}", aggregation.high());
+        append_to_buffer!(buf, "low:            {}", aggregation.low());
+        append_to_buffer!(buf, "open:           {}", aggregation.open());
+        append_to_buffer!(buf, "close:          {}", aggregation.close());
         Some(buf)
     }
 }
