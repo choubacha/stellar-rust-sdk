997ca95def4e1df885fc51e0ff76ab090a63117e
Kevin Choubacha <chewbacha@gmail.com>
2018-04-30 19:57:38 -0700

    Implement operations command
    This implements the all operations and the operation effects commands
    for the CLI
    
      closes #157 and #162

---
+++ cli/src/operations.rs
@@ -0,0 +1,49 @@
+use stellar_client::{endpoint::operation, sync::{self, Client}};
+use clap::ArgMatches;
+use super::{cursor, ordering, pager::Pager};
+use error::Result;
+use fmt::{Formatter, Simple};
+
+pub fn all(client: &Client, matches: &ArgMatches) -> Result<()> {
+    let pager = Pager::from_arg(&matches);
+
+    let endpoint = operation::All::default();
+    let endpoint = pager.assign(endpoint);
+    let endpoint = cursor::assign_from_arg(matches, endpoint);
+    let endpoint = ordering::assign_from_arg(matches, endpoint);
+
+    let iter = sync::Iter::new(&client, endpoint);
+
+    let mut res = Ok(());
+    let mut fmt = Formatter::start_stdout(Simple);
+    pager.paginate(iter, |result| match result {
+        Ok(operation) => fmt.render(&operation),
+        Err(err) => res = Err(err.into()),
+    });
+    let _ = fmt.stop();
+    res
+}
+
+pub fn effects(client: &Client, matches: &ArgMatches) -> Result<()> {
+    let pager = Pager::from_arg(&matches);
+    let id: i64 = matches
+        .value_of("ID")
+        .expect("Operation id is required")
+        .parse()
+        .map_err(|_| String::from("Operation ID must be a valid 64-bit integer"))?;
+
+    let endpoint = operation::Effects::new(id);
+    let endpoint = pager.assign(endpoint);
+    let endpoint = cursor::assign_from_arg(matches, endpoint);
+    let endpoint = ordering::assign_from_arg(matches, endpoint);
+
+    let iter = sync::Iter::new(&client, endpoint);
+
+    let mut res = Ok(());
+    let mut fmt = Formatter::start_stdout(Simple);
+    pager.paginate(iter, |result| match result {
+        Ok(effect) => fmt.render(&effect),
+        Err(err) => res = Err(err.into()),
+    });
+    res
+}
